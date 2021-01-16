137a35438de4367268c20fe2f35e4fdef865382e
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-01-15 18:46:09 -0600

    first file written

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## ${data.email}
+  ## ${data.gitHub}
+  ## ${data.repository}
+  ## ${data.description}
 
 `;
 }
